94b2ba7ce2cc7ca0757a4b1b98700cdab929a739
vineethdasi <112347129+vineethdasi@users.noreply.github.com>
2022-09-05 23:08:55 -0400

    added Sym.py

---
+++ code/Sym.py
@@ -0,0 +1,33 @@
+import math
+from LuaCode import *
+
+class Sym:
+    def __init__(self,c=0,s=""):
+        self.n=0
+        self.c=c
+        self.name=s
+        self._has=[]
+
+    def add(self,v):
+        if v!= "?":
+            self.n= self.n+1
+            self._has[v] = 1+ self._has.get(v, 0)
+
+    def mid(self,col,most,mode):
+        most=-1
+        for k,v in self._has.items():
+            if v>most:
+                mode,most=k,v
+        return mode
+    
+    def div(self,e,fun):
+        def fun(p):
+            return p*math.log(p,2)
+        e=0
+        for _,n in self._has.items():
+            if n>0:
+                e=e-fun(n/self.n)
+        return e
+
+    
+
