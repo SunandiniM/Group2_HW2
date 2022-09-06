5cc4b72706ba1f4a017bec048b05af9939143c0c
SahithiAmmana <112002925+SahithiAmmana@users.noreply.github.com>
2022-09-05 23:15:42 -0400

    Add utilities.py

---
+++ code/utilities.py
@@ -0,0 +1,43 @@
+from fileinput import filename
+from LuaCode import *
+import re
+
+def csv():
+    def fun(s):
+        if s=="true":
+            return True
+        elif s=="false":
+            return False
+        elif re.match(r"^[0-9]+$",s):
+            return int(s)
+    sep = "([^"+the.seperator+"]+)"
+    src = io.open(filename)
+    while True:
+        s = io.read()
+        if not s:
+            return io.close(src)
+        t = []
+        for s1 in s.match(sep):
+            t.append(coerce(s1))
+        
+        fun(t)
+
+def o(t):
+    if type(t) is not dict:
+        return str(t)
+
+    def show(k,v):
+        if not str(k).find("^_"):
+            v=o(v)
+            return len(t)==0 and ":{} {}".format(k,v) or str(v)
+
+    u=[]
+    for k,v in t.items():
+        u[1+len(u)]=show(k,v)
+    if len(t)==0:
+        u.sort()
+    return "{"+' '.join([str(item) for item in u])+"}"
+
+def oo(t):
+    print(o(t))
+    return t
