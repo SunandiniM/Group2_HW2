7e6a3b03575c024bb438dc5586cf6b03edc178d6
SunandiniM <smedise@ncsu.edu>
2022-09-12 21:33:19 -0400

    Adds data.py file

--- code/Data.py
+++ code/Data.py
@@ -13,9 +13,9 @@ class Data:
     def add(self, xs):
       if not self.cols:
         self.cols = Cols(xs)
-    else:
-      row = push(self.rows, Row(row))
-        for todo in [self.cols.x, self.cols.y]:
-          for col in todo:
-            col.add(row.cells[col.at])
+      else:
+        row = push(self.rows, Row(row))
+          for todo in [self.cols.x, self.cols.y]:
+            for col in todo:
+              col.add(row.cells[col.at])
         
